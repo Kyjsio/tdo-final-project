a2dee7917047c8c16ceb939a8e4d919fe1be0dd0
Ilona <minilegof@gmail.com>
2026-01-23 02:03:13 +0100

    Secret key

--- app/config.py
+++ app/config.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 class Settings:
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
