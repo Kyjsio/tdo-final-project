2f98ac614b268b4e23fe599eb0d389a6962f4053
Ilona <minilegof@gmail.com>
2026-01-23 02:21:29 +0100

    Add dependencies

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from pathlib import Path
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from app.routers import books
+from app.routers import books, auth
 from app.database import Base, engine
 import os
 from prometheus_fastapi_instrumentator import Instrumentator
@@ -18,7 +18,7 @@ static_dir = os.path.join(BASE_DIR, "static")
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 app.include_router(books.router)
-
+app.include_router(auth.router)
 
 @app.on_event("startup")
 async def startup_event():
