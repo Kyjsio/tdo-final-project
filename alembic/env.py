6b27af96c2af9f5a3664a809013c1b362bef7080
Gabiwalcz7 <s171282@student.uken.krakow.pl>
2026-01-24 14:02:56 +0100

    keep alembic model import (noqa for ruff)

--- alembic/env.py
+++ alembic/env.py
@@ -1,13 +1,12 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from sqlalchemy import create_engine
 
 from alembic import context
 from app.config import settings
 from app.database import Base
-import app.models  
+import app.models # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
